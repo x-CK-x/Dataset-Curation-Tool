a7ba5369b71baf47849a422527fbe2038d588a18
(CK) <48079849+x-CK-x@users.noreply.github.com>
2025-07-01 16:08:54 -0400

    Add option to disable tag suggestions

--- UI_tabs/advanced_settings_tab.py
+++ UI_tabs/advanced_settings_tab.py
@@ -3,8 +3,17 @@ import gradio as gr
 class Advanced_settings_tab:
     def render_tab(self):
         with gr.Tab("Advanced Settings"):
-            total_suggestions_slider = gr.Slider(info="Limit Number of Tag Suggestions", minimum=0, maximum=1000, step=1, value=10, show_label=False)
+            tag_suggestions_checkbox = gr.Checkbox(label="Enable Tag Suggestions", value=True)
+            total_suggestions_slider = gr.Slider(
+                info="Limit Number of Tag Suggestions",
+                minimum=0,
+                maximum=1000,
+                step=1,
+                value=10,
+                show_label=False,
+            )
 
+        self.tag_suggestions_checkbox = tag_suggestions_checkbox
         self.total_suggestions_slider = total_suggestions_slider
 
-        return self.total_suggestions_slider
+        return self.total_suggestions_slider, self.tag_suggestions_checkbox
